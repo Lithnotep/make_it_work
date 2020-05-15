0572c940b0414902f977488dd0a01ac291c33713
Max Mitrani <mjmitrani168@gmail.com>
2020-05-15 09:32:41 -0600

    Add Projects controller and show page

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -12,15 +12,3 @@ RSpec.describe "When a user visits project show page" do
     expect(page).to have_content(recycled_material_challenge.theme)
   end
 end
-
-
-
-
-
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
